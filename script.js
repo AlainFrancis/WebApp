04c6c466165f923a22b0f45c63a3521f596f9566
alain <alainfrancis2001@gmail.com>
2023-11-15 09:33:09 +0200

    add new files

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("clickMeButton").addEventListener("click", function() {
+    var outputElement = document.getElementById("output");
+    outputElement.textContent = "Button clicked!";
+});
